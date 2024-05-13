f3fe2a04cea95951e54894805ca1bc8e5409dfce
JcTheSavior <jacobconnell3@gmail.com>
2024-05-13 02:40:04 -0400

    Fetch data EachSpell page, style work in progress
    Fetch request for data successfully, iterator needs to be defined for
    different set of data

--- src/pages/EachSpell.tsx
+++ src/pages/EachSpell.tsx
@@ -1,6 +1,45 @@
-const EachSpell = () => {
-  return (
-    <div>EachSpell</div>
-  )
+import {useState, useEffect} from "react";
+import {useParams} from "react-router-dom"
+
+interface ISpell {
 }
-export default EachSpell
+interface IPicked {
+
+}
+
+const EachSpell = () => {
+  const params = useParams();
+  const symbol = params.symbol
+  const url = `https://www.dnd5eapi.co/api/spells/${symbol?.toLowerCase()}`
+
+  const [picked, setPicked] = useState<IPicked | null>(null);
+
+  const getSpell = async () => {
+    try {
+      const response = await fetch(url);
+      const data = await response.json();
+      setPicked(data) 
+      console.log(data)
+    } catch(e) {
+      console.error(e)
+    }
+  };
+  useEffect(() => {
+    getSpell();
+  }, [])
+
+  const loaded = () => {
+    console.log(picked)
+    return (
+      <div className="underConstruction">
+        <div className="underConstructionImage"></div> 
+      </div>
+    )
+  }
+const loading = () => {
+  return <h1>Loading...</h1>;
+};
+
+  return picked ? loaded() : loading();
+}
+export default EachSpell;
