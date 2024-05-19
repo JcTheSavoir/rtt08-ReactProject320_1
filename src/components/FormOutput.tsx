adcc739f0b3b67259cbcc512ad3c2807244cf07a
JcTheSavior <jacobconnell3@gmail.com>
2024-05-19 14:53:08 -0400

    Add; component w/ conditional for FormOutput
    Form output will accept a valid monster from the users output, and
    pass the data for that monster as a prop to the FormOutput

---
+++ src/components/FormOutput.tsx
@@ -0,0 +1,12 @@
+import { Details } from "../interfaces/searchMonstersInter";
+
+const FormOutput = ({ theSearchedMonster }: { theSearchedMonster: Details | null }) => {
+    console.log(theSearchedMonster)
+  return (
+    <div className="formOutputContainer">
+
+
+    </div>
+  )
+}
+export default FormOutput
--- src/pages/SearchMonster.tsx
+++ src/pages/SearchMonster.tsx
@@ -1,6 +1,7 @@
-import {useState, useEffect} from "react";
+import {useState, useEffect, useRef} from "react";
 import {useParams} from "react-router-dom"
-import { Speed, Skills, Actions, Details, IMonsters } from "../interfaces/searchMonstersInter";
+import { IMonsters } from "../interfaces/searchMonstersInter";
+import FormOutput from "../components/FormOutput";
 
 const SearchMonster = () => {
   const params = useParams();
@@ -9,25 +10,20 @@ const SearchMonster = () => {
   // some parameters are prefilled in order to limit searchability; in order to limit amount of localstorage being used
   const url = `https://api.open5e.com/v1/${symbol?.toLowerCase()}/?document__slug=wotc-srd&format=json&limit=350`
 
+  // ---------------------  states and refs 
   const [monsters, setMonsters] = useState<IMonsters | null>(null);
   const [searchInput, setSearchInput] = useState("");
-
-  const getSearch = () => {
-    
-  }
-
+  const inputReference = useRef<HTMLInputElement>(null); 
+  
   const submitForm = (event: React.FormEvent<HTMLFormElement>) => {
-    console.log("search button clicked")
     event.preventDefault()
   }
 
   const getMonsters = async () => {
     try {
-      console.log("Getting Monsters")
       const response = await fetch(url);
       const data = await response.json();
       setMonsters(data) 
-      console.log(data)
     } catch(e) {
       console.error(e)
     }
@@ -36,32 +32,60 @@ const SearchMonster = () => {
   useEffect(() => {
     getMonsters();
   }, [])
-  const namesArray = []
-
+  
+  // keeping track of the input as it's changed (by user typing or selecting an option)
   const inputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setSearchInput(event.target.value);
+    const value = event.target.value
+    setSearchInput(value);
   };
-
+  
+  //Array to hold the different "monster names" returned by the api
+  let namesArray: string[] = []
+  
   const loaded = () => {
+    
+    // Add's all of the monster names to the namesArray
+    const allMonsterNames = monsters?.results.map((monster) =>
+      namesArray.push(monster.name)
+    )
+
     const filterMonsters = monsters?.results.filter(
       monster => monster.name.toLowerCase().includes(searchInput.toLowerCase())
     ).slice(0, 10) || [];
-    console.log(filterMonsters)
+
+    const getSearch = () => {
+      if (inputReference.current) {
+        const currentMonster = inputReference.current.value;
+        if (namesArray.includes(currentMonster)) {
+          const foundMonster = filterMonsters.find(Details => Details.name === currentMonster)
+          return(foundMonster)
+        }
+      };
+      return null; //If no monster is found from the users input
+    }
+
+    // set this to the return of getSearch, will be passed as a prop
+    const searchedMonster = getSearch()
 
     return (
       <div className="apiContainer">
-        <form onSubmit={submitForm} action="">
+        <form onSubmit={submitForm} className="apiFormContainer">
           <label htmlFor="searchName">Enter a Monsters Name: </label>
-          <input list="monsterNames" name="searchName" value={searchInput} onChange={inputChange}/>
+          <input list="monsterNames" name="searchName" ref={inputReference} value={searchInput} onChange={inputChange}/>
           <datalist id="monsterNames">
-            {filterMonsters?.map((monster) => (
+            {filterMonsters?.map((monster) => {
+              return(
               <option key={monster.slug} value={monster.name}></option>
-            ))}
+            )})}
           </datalist>
-          <br/>
-          <button onClick={getSearch}>Submit</button>
+          <button onClick={getSearch} className="apiFormButton">Submit</button>
         </form>
-        <h1></h1>
+          {searchedMonster ? (
+              <FormOutput theSearchedMonster={searchedMonster} />
+            ) : (
+              <h2>No monster found by that name, try searching again.  You can try "Zombie" as a known working option</h2>
+            )
+          }
       </div>
     )
   }
