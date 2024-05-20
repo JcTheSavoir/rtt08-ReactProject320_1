1a3e91a4c00f63dbb34a443ea1ce67aa25869234
JcTheSavior <jacobconnell3@gmail.com>
2024-05-20 00:42:24 -0400

    Fix commit;Refactor to use createBrowserRouter Good Repository explaining this (and has a youtube video link on it's readme file) is "https://github.com/ErickKS/vite-deploy?tab=readme-ov-file"

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,7 @@ import './style.css'
 
 // import { useState } from 'react';
 import { Outlet } from 'react-router-dom';
-import Homepage from './pages/Homepage';
-import Options from './pages/Options';
-import SearchMonster from './pages/SearchMonster'
-import AllSpells from './pages/AllSpells';
 import NavBar from './components/NavBar';
-import EachSpell from './pages/EachSpell';
 
 function App() {
   // const [count, setCount] = useState(0)
